6040196e5e5b40f92286982dd3cbfff5df1d37a0
benhur-araujo <benhur.araujo.silva@gmail.com>
2024-11-12 07:22:36 -0300

    feat(disk-analyzer): improve summary & add cleanup feature

--- disk-analyzer/disk-analyzer.sh
+++ disk-analyzer/disk-analyzer.sh
@@ -7,11 +7,13 @@ usage() {
 Usage: ./$0 [OPTION]...
 
 Options:
-  -d  specify the directory to be analyzed. Default: /home
+  -d  specify the directory to be analyzed.
   -r  set recursively level. Default: 0
+  -s  show disk summary.
+  -c  clean unnecessary logs, files & apt packages.
 
 Examples:
-  ./disk-analyzer              # Will analyze /home
+  ./disk-analyzer              # Show disk summary
   ./disk-analyzer -d /tmp      # Will analyze /tmp
   ./disk-analyzer -r 1 -d /tmp # Will analyze /tmp and /tmp/*
 
@@ -26,8 +28,24 @@ max_depth=0
 disk_summary() {
     echo "### Disks Summary  ###"
     df -h
+    
     echo -e "\n### Root level consumption ###"
     du -hs -t 1m /* 2> /dev/null | sort -h
+
+    local unused_packages="$(sudo apt-get autoremove -s | grep "disk space will be freed")"
+    if [[ "$unused_packages" ]]; then
+        echo -e "\n # Unused apt packages and old kernels #"
+        echo "$unused_packages"
+    fi
+
+    echo -e "\n Disk spaced used by apt cache"
+    sudo du -sh /var/cache/apt
+
+    echo -e "\n # Disk space used by logs #"
+    journalctl --disk-usage
+
+    echo -e "\n # Disk space used by old snap revisions"
+    du -hs /var/lib/snapd/snaps
 }
 
 dir_size() {
@@ -35,10 +53,27 @@ dir_size() {
     du -hd "$1" "$2" | sort -h
 }
 
+clean_up() {
+    echo -e "\n # Removing unused apt packages and old kernels..."
+    sudo apt autoremove
+    sudo apt clean
+    
+    echo -e "\n # Deleting system logs older than 3d..."
+    sudo journalctl --vacuum-time=3d
+
+    echo -e "\n # Deleting old revisions of snaps..."
+    snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        sudo snap remove "$snapname" --revision="$revision"
+    done
+}
+
 # Processing options
-while getopts "r:d:s" opt; do
+clean="false"
+while getopts "r:d:sc" opt; do
     case "$opt" in
         s) disk_summary;;
+        c) clean="true";;
         r) max_depth="$OPTARG";;
         d) dir="$OPTARG";;
         \?) usage;;
@@ -49,6 +84,11 @@ while getopts "r:d:s" opt; do
 main () {
     if [[ "$#" -eq 0 ]]; then
         disk_summary
+        exit 1
+    fi
+    
+    if [[ $clean == "true" ]]; then
+        clean_up
         exit 1
     fi
     
